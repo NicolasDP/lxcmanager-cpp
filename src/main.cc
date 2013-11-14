015667b50ee4f6ca3123cc53689e6a07ebd357ec
Nicolas DI PRIMA <nicolas@di-prima.fr>
2013-11-14 23:53:03 +0000

    [SRC] main.cc: compilation fix (include config.hh instead of config.h)

--- src/main.cc
+++ src/main.cc
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with LXCManager.  If not, see <http://www.gnu.org/licenses/>. */
 
-#include "config.h"
+#include "config.hh"
 #include "modules.hh"
 
 int main ()
