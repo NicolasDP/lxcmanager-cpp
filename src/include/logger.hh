610a96acfa0a854b91ae04ab20aaf7a2a9e1f4d0
Nicolas DI PRIMA <nicolas@di-prima.fr>
2013-11-17 11:29:41 +0000

    [LOGGER] add comments and change the indentation

--- src/include/logger.hh
+++ src/include/logger.hh
@@ -1,3 +1,18 @@
+/* This file is part of LXCManager.
+ *
+ * LXCManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LXCManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LXCManager.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef LOGGER_HH_
 # define LOGGER_HH_
 
@@ -6,34 +21,64 @@
 
 # include "options.hh"
 
+/*!
+ * @class LXCMLogger
+ *
+ * Logger interfaces, it is a tool to log messages (ERROR, INFO, DEBUG...).
+ * Example:
+ *   LXCMLogger::log (LXCMLogger::ERROR, "this is an error message");
+ *
+ * More information about how to setup the message log mask or the log message
+ * destination (a file instead of the std::log output) see the constructor. */
 class LXCMLogger : public LXCMCoreModule
 {
-	public:
-		typedef
-		enum
-		{
-			ERROR = 0,
-			INFO,
-			DEBUG,
+  public:
+    /*! @enum level
+     * It defines the log message levels */
+    typedef
+    enum
+      {
+        ERROR = 0,
+        INFO,
+        DEBUG,
 
-			NUMBER_OF_LOG_LEVEL /* NEVER USE IT or upper number */
-		}
-		level;
+        NUMBER_OF_LOG_LEVEL /* NEVER USE IT or upper number */
+      }
+    level;
 
-	public:
-		static void init ();
-		~LXCMLogger ();
+  public:
+    /*! @brief init initialize the singleton instance */
+    static void init ();
+    /*! @brief destructor */
+    ~LXCMLogger ();
 
-		static void log (level const, std::string const&);
-		OptionsParseCode checkOptions (po::variables_map&);
+    /*! @brief log
+     * This function gives the main interfaces to throw a log message. It will
+     * be printed in the standard log output (std::clog) or in a file given by
+     * the option command line (--log-file path/to/log).
+     *
+     * @param level the level of this debug message
+     * @param std::string the log message */
+    static void log (level const, std::string const&);
+    /*! @brief checkOptions
+     * it implements the virtual method defined in the
+     * LXCMCoreModule. */
+    OptionsParseCode checkOptions (po::variables_map&);
 
-	private:
-		LXCMLogger ();
-		static LXCMLogger* _singleton;
+  private:
+    /*! @brief constructor */
+    LXCMLogger ();
+    /*! @brief the unique instance of this Class */
+    static LXCMLogger* _singleton;
 
-	private:
-		level _maxLevel;
-		std::ofstream _logOutput;
+  private:
+    /*! @brief the log message mask level, modified by the command line option
+     * --log-level. */
+    level _maxLevel;
+    /*! @brief the log file, setted by the command line --log-file. If this
+     * option is not given then the log message will be printed in the standard
+     * log output (std::clog). */
+    std::ofstream _logOutput;
 };
 
 #endif /* !LOGGER_HH_ */
--- src/logger.cc
+++ src/logger.cc
@@ -1,3 +1,18 @@
+/* This file is part of LXCManager.
+ *
+ * LXCManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LXCManager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LXCManager.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <exception>
 #include <iostream>
 #include <ctime>
@@ -10,106 +25,106 @@ LXCMLogger* LXCMLogger::_singleton = NULL;
 
 static std::string const level_header[LXCMLogger::NUMBER_OF_LOG_LEVEL] =
 {
-	[LXCMLogger::ERROR] = "ERROR",
-	[LXCMLogger::INFO]  = "INFO",
-	[LXCMLogger::DEBUG] = "DEBUG"
+  [LXCMLogger::ERROR] = "ERROR",
+  [LXCMLogger::INFO]  = "INFO",
+  [LXCMLogger::DEBUG] = "DEBUG"
 };
 
 LXCMLogger::LXCMLogger () : _logOutput ()
 {
-	LXCMOptions* opt = LXCMOptions::getOptions ();
+  LXCMOptions* opt = LXCMOptions::getOptions ();
 
-	this->_maxLevel = LXCMLogger::ERROR;
+  this->_maxLevel = LXCMLogger::ERROR;
 
-	opt->addModule (this);
-	opt->addOption ("log-level",
-	                po::value<int> (),
-	                "set log level mask:\n"
-	                "    0: Errors Only\n"
-	                "    1: Infos and Errors only\n"
-	                "    2: Debug, Infos and Errors");
-	opt->addOption ("log-file",
-	                po::value<std::string> (),
-	                "set log file to write all of the log messages. If "
-	                "not set, log messages are redirected to the error "
-	                "output");
+  opt->addModule (this);
+  opt->addOption ("log-level",
+		  po::value<int> (),
+		  "set log level mask:\n"
+		  "    0: Errors Only\n"
+		  "    1: Infos and Errors only\n"
+		  "    2: Debug, Infos and Errors");
+  opt->addOption ("log-file",
+		  po::value<std::string> (),
+		  "set log file to write all of the log messages. If "
+		  "not set, log messages are redirected to the error "
+		  "output");
 
-	this->_moduleName = "LXCMLXCMLogger";
+  this->_moduleName = "LXCMLXCMLogger";
 }
 
 LXCMLogger::~LXCMLogger ()
 {
-	this->_logOutput.close ();
+  this->_logOutput.close ();
 }
 
 void LXCMLogger::init ()
 {
-	if (!LXCMLogger::_singleton)
-	{
-		LXCMLogger::_singleton = new LXCMLogger ();
-	}
+  if (!LXCMLogger::_singleton)
+  {
+    LXCMLogger::_singleton = new LXCMLogger ();
+  }
 }
 
 OptionsParseCode LXCMLogger::checkOptions (po::variables_map& vm)
 {
-	if (vm.count ("log-level"))
-	{
-		int lvl;
-		/* get the default value and do some update */
-		lvl = vm["log-level"].as<int> ();
-		switch (lvl)
-		{
-		case ERROR:
-		case INFO:
-		case DEBUG:
-			this->_maxLevel = (LXCMLogger::level) lvl;
-			break;
-		case NUMBER_OF_LOG_LEVEL:
-		default:
-			return ERR_ERROR;
-		}
-	}
+  if (vm.count ("log-level"))
+  {
+    int lvl;
+    /* get the default value and do some update */
+    lvl = vm["log-level"].as<int> ();
+    switch (lvl)
+    {
+      case ERROR:
+      case INFO:
+      case DEBUG:
+	this->_maxLevel = (LXCMLogger::level) lvl;
+	break;
+      case NUMBER_OF_LOG_LEVEL:
+      default:
+	return ERR_ERROR;
+    }
+  }
 
-	if (vm.count ("log-file"))
-	{
-		std::string filename;
-		filename = vm["log-file"].as<std::string> ();
-		std::cerr << "open (" << filename << ")" << std::endl;
-		/* Open the file */
-		try
-		{
-			this->_logOutput.open (filename.c_str (),
-			               std::ofstream::out | std::ofstream::app);
+  if (vm.count ("log-file"))
+  {
+    std::string filename;
+    filename = vm["log-file"].as<std::string> ();
+    std::cerr << "open (" << filename << ")" << std::endl;
+    /* Open the file */
+    try
+    {
+      this->_logOutput.open (filename.c_str (),
+			     std::ofstream::out | std::ofstream::app);
 
-		}
-		catch (std::exception& e)
-		{
-			this->log (LXCMLogger::ERROR, e.what ());
-		}
-	}
+    }
+    catch (std::exception& e)
+    {
+      this->log (LXCMLogger::ERROR, e.what ());
+    }
+  }
 
-	return ERR_NONE;
+  return ERR_NONE;
 }
 
 void LXCMLogger::log (LXCMLogger::level const lvl, std::string const& message)
 {
-	if (lvl <= LXCMLogger::_singleton->_maxLevel)
-	{
-		if (LXCMLogger::_singleton->_logOutput.is_open ())
-		{
-			std::chrono::system_clock::time_point now = std::chrono::system_clock::now ();
-			std::time_t tt = std::chrono::system_clock::to_time_t (now);
-			LXCMLogger::_singleton->_logOutput
-			    << "[" << tt << "]"
-			    << "[" << PROJECT_NAME << "]"
-			    << "[" << level_header[lvl] << "] "
-			    << message << std::endl;
-		}
-		else
-		{
-			std::clog
-			    << "[" << level_header[lvl] << "] "
-			    << message << std::endl;
-		}
-	}
+  if (lvl <= LXCMLogger::_singleton->_maxLevel)
+  {
+    if (LXCMLogger::_singleton->_logOutput.is_open ())
+    {
+      std::chrono::system_clock::time_point now = std::chrono::system_clock::now ();
+      std::time_t tt = std::chrono::system_clock::to_time_t (now);
+      LXCMLogger::_singleton->_logOutput
+	<< "[" << tt << "]"
+	<< "[" << PROJECT_NAME << "]"
+	<< "[" << level_header[lvl] << "] "
+	<< message << std::endl;
+    }
+    else
+    {
+      std::clog
+	<< "[" << level_header[lvl] << "] "
+	<< message << std::endl;
+    }
+  }
 }
