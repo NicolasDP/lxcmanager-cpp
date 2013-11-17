812bf123c4340cd38eec857d335d7f924e5a1a12
Nicolas DI PRIMA <nicolas@di-prima.fr>
2013-11-17 11:52:58 +0000

    [OPTIONS] change indentation

--- src/include/options.hh
+++ src/include/options.hh
@@ -18,11 +18,11 @@ namespace po = boost::program_options;
 typedef
 enum OptionsParseCode
 {
-	ERR_NONE = 0x0, /*!< no error, continue */
-	ERR_HELP,       /*!< no error, but user requested for something else
-	                 * which needs to leave without executing the program */
-	ERR_ERROR       /*!< an error occured, leave the program as soon as
-	                 * possible */
+  ERR_NONE = 0x0, /*!< no error, continue */
+  ERR_HELP,       /*!< no error, but user requested for something else
+                   * which needs to leave without executing the program */
+  ERR_ERROR       /*!< an error occured, leave the program as soon as
+                   * possible */
 }
 OptionsParseCode;
 
@@ -35,72 +35,72 @@ OptionsParseCode;
  * It's implement an options module */
 class LXCMOptions : public LXCMCoreModule
 {
-	public:
-		/*! @brief getOptions
-		 *  returns the LXCMOptions instance and initializes it if
-		 *  needed.
-		 *  @return LXCMOptions, the unique instance of this Class */
-		static LXCMOptions* getOptions ();
-		/*! @brief destructor */
-		~LXCMOptions ();
+  public:
+    /*! @brief getOptions
+     *  returns the LXCMOptions instance and initializes it if
+     *  needed.
+     *  @return LXCMOptions, the unique instance of this Class */
+    static LXCMOptions* getOptions ();
+    /*! @brief destructor */
+    ~LXCMOptions ();
 
-		/*! @brief addModule
-		 *  it saves the instance LXCModule address in the _modules
-		 *  deque. At the parsing time, the parseOptions methode will
-		 *  be call for each registered LXCModule.
-		 *
-		 *  @param LXCModule*: the address of a LXCModule instance */
-		void addModule (LXCMCoreModule*);
-		/*! @brief addOption
-		 *  it adds an option in the boost::program_options
-		 *  description. This option doesn't expect an argement.
-		 *
-		 *  @param char const*: the option string "long,s" where long
-		 *  is a long option (--long) and s is the associated shord
-		 *  option (-s).
-		 *  @param char const*: the option description */
-		void addOption (char const*, char const*);
-		/*! @brief addOption
-		 *  it does the same but with an argument required.
-		 *
-		 *  @param char const*: the option string "long,s" where long
-		 *  is a long option (--long) and s is the associated shord
-		 *  option (-s).
-		 *  @param po::value_semantic which defines the type of a
-		 *  required argement. See boost::program_option for more
-		 *  information.
-		 *  @param char const*: the option description */
-		void addOption (char const*, po::value_semantic const*, char const*);
+    /*! @brief addModule
+     *  it saves the instance LXCModule address in the _modules
+     *  deque. At the parsing time, the parseOptions methode will
+     *  be call for each registered LXCModule.
+     *
+     *  @param LXCModule*: the address of a LXCModule instance */
+    void addModule (LXCMCoreModule*);
+    /*! @brief addOption
+     *  it adds an option in the boost::program_options
+     *  description. This option doesn't expect an argement.
+     *
+     *  @param char const*: the option string "long,s" where long
+     *  is a long option (--long) and s is the associated shord
+     *  option (-s).
+     *  @param char const*: the option description */
+    void addOption (char const*, char const*);
+    /*! @brief addOption
+     *  it does the same but with an argument required.
+     *
+     *  @param char const*: the option string "long,s" where long
+     *  is a long option (--long) and s is the associated shord
+     *  option (-s).
+     *  @param po::value_semantic which defines the type of a
+     *  required argement. See boost::program_option for more
+     *  information.
+     *  @param char const*: the option description */
+    void addOption (char const*, po::value_semantic const*, char const*);
 
-		/*! @brief parseOptions
-		 *  it parses all of the registered options from the registered
-		 *  module (see addOption and addModule functions).
-		 *
-		 *  @param int const: the number of argument given in command
-		 *  line option (argc).
-		 *  @param char const* const* the argument array givent in
-		 *  command line option (argv).
-		 *
-		 *  @return OptionsParseCode
-		 *  see OptionsParseCode */
-		OptionsParseCode parseOptions (int const, char const* const*);
-		/*! @brief checkOptions (see LXCModule) */
-		OptionsParseCode checkOptions (po::variables_map&);
+    /*! @brief parseOptions
+     *  it parses all of the registered options from the registered
+     *  module (see addOption and addModule functions).
+     *
+     *  @param int const: the number of argument given in command
+     *  line option (argc).
+     *  @param char const* const* the argument array givent in
+     *  command line option (argv).
+     *
+     *  @return OptionsParseCode
+     *  see OptionsParseCode */
+    OptionsParseCode parseOptions (int const, char const* const*);
+    /*! @brief checkOptions (see LXCModule) */
+    OptionsParseCode checkOptions (po::variables_map&);
 
-	private:
-		/*! @brief constructor */
-		LXCMOptions ();
-		/*! @brief singleton field (unique value) see Singleton design
-		 * pattern */
-		static LXCMOptions* _singleton;
-	private:
-		/*! @brief see bosst::program_option */
-		po::options_description* _desc;
-		/*! @brief see bosst::program_option */
-		po::variables_map*       _vm;
+  private:
+    /*! @brief constructor */
+    LXCMOptions ();
+    /*! @brief singleton field (unique value) see Singleton design
+     * pattern */
+    static LXCMOptions* _singleton;
+  private:
+    /*! @brief see bosst::program_option */
+    po::options_description* _desc;
+    /*! @brief see bosst::program_option */
+    po::variables_map*       _vm;
 
-		/*! @brief the instance of each registered LXCModule */
-		std::deque<LXCMCoreModule*>* _modules;
+    /*! @brief the instance of each registered LXCModule */
+    std::deque<LXCMCoreModule*>* _modules;
 };
 
 #endif /* !LXCMOPTIONS_HH_ */
--- src/options.cc
+++ src/options.cc
@@ -5,91 +5,91 @@ LXCMOptions* LXCMOptions::_singleton = NULL;
 
 LXCMOptions::LXCMOptions ()
 {
-	this->_desc = new po::options_description ("Generic options");
-	this->_vm = new po::variables_map ();
+  this->_desc = new po::options_description ("Generic options");
+  this->_vm = new po::variables_map ();
 
-	this->_modules = new std::deque<LXCMCoreModule*> ();
+  this->_modules = new std::deque<LXCMCoreModule*> ();
 
-	this->_desc->add_options ()
-	    ("help,h", "produce help message");
+  this->_desc->add_options ()
+    ("help,h", "produce help message");
 
-	this->_moduleName = "LXCMLXCMOptions";
+  this->_moduleName = "LXCMLXCMOptions";
 }
 
 LXCMOptions::~LXCMOptions ()
 {
-	delete this->_desc;
-	delete this->_vm;
+  delete this->_desc;
+  delete this->_vm;
 }
 
 LXCMOptions* LXCMOptions::getOptions ()
 {
-	if (!LXCMOptions::_singleton)
-	{
-		LXCMOptions::_singleton = new LXCMOptions ();
-		LXCMOptions::_singleton->addModule (LXCMOptions::_singleton);
-	}
+  if (!LXCMOptions::_singleton)
+  {
+    LXCMOptions::_singleton = new LXCMOptions ();
+    LXCMOptions::_singleton->addModule (LXCMOptions::_singleton);
+  }
 
-	return LXCMOptions::_singleton;
+  return LXCMOptions::_singleton;
 }
 
 OptionsParseCode LXCMOptions::checkOptions (po::variables_map& vm)
 {
-	if (vm.count ("help"))
-	{
-		std::cout << *this->_desc << std::endl;
-		return ERR_HELP;
-	}
+  if (vm.count ("help"))
+  {
+    std::cout << *this->_desc << std::endl;
+    return ERR_HELP;
+  }
 
-	return ERR_NONE;
+  return ERR_NONE;
 }
 
 void LXCMOptions::addModule (LXCMCoreModule* module)
 {
-	if (module)
-	{
-		this->_modules->push_back (module);
-	}
+  if (module)
+  {
+    this->_modules->push_back (module);
+  }
 }
 
 void LXCMOptions::addOption (char const* name, char const* desc)
 {
-	this->_desc->add_options () (name, desc);
+  this->_desc->add_options () (name, desc);
 }
 
 void LXCMOptions::addOption (char const* n, po::value_semantic const* v, char const* d)
 {
-	this->_desc->add_options () (n, v, d);
+  this->_desc->add_options () (n, v, d);
 }
 
 OptionsParseCode LXCMOptions::parseOptions (int const argc, char const* const* argv)
 {
-	std::deque<LXCMCoreModule*>::iterator it;
-	std::deque<LXCMCoreModule*>::iterator end;
-	OptionsParseCode ret = ERR_NONE;
+  std::deque<LXCMCoreModule*>::iterator it;
+  std::deque<LXCMCoreModule*>::iterator end;
+  OptionsParseCode ret = ERR_NONE;
 
-	try
-	{
-		po::store (po::parse_command_line (argc, argv, *this->_desc), *this->_vm);
-		po::notify (*this->_vm);
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what () << std::endl;
-		return ERR_ERROR;
-	}
+  try
+  {
+    po::store (po::parse_command_line (argc, argv, *this->_desc), *this->_vm);
+    po::notify (*this->_vm);
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what () << std::endl;
+    return ERR_ERROR;
+  }
 
-	end = this->_modules->end ();
-	for (it = this->_modules->begin (); it != end; it++)
-	{
-		LXCMLogger::log (LXCMLogger::INFO, (*it)->moduleName ());
-		ret = (*it)->checkOptions (*this->_vm);
-		if (ERR_NONE != ret)
-		{
-			break;
-		}
-	}
+  end = this->_modules->end ();
+  for (it = this->_modules->begin (); it != end; it++)
+  {
+    LXCMLogger::log (LXCMLogger::INFO, (*it)->moduleName ());
+    ret = (*it)->checkOptions (*this->_vm);
+    if (ERR_NONE != ret)
+    {
+      break;
+    }
+  }
 
-	return ret;
+  return ret;
 }
 
